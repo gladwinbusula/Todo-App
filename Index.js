2e85194c9fdd2f18e9e55558bac146ff4b490d41
gladwinbusula <gladwinbusula32@gmail.com>
2023-02-02 14:57:12 +0300

    add file

---
+++ Index.js
@@ -0,0 +1,26 @@
+
+const taskinput= document.querySelector(".task-input input");
+taskinput.addEventListesner("keyup"){
+   let userTask=taskinput.ariaValueMax.trim();
+   if(key== "enter" && userTask){
+      //getting localstorage todo list
+      let todos=localStorage.getItem("todo list");
+
+   }
+}
+todo.push(taskinf) //adding new task
+localStorage.setitem("todo.list", String(todos));
+    
+   
+    
+ 
+   
+
+ 
+
+ 
+
+
+
+
+
